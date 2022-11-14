f5bf1caf8ff5b143e5716432e7e62bb14e6184e5
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2022-11-15 02:20:55 +0300

    Initial Commit

---
+++ HW4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW4
+//
+//  Created by Xiaomi on 15.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
